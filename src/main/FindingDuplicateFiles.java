00575df16c7c84abde31b99ee24b117c2ef8ebae
idivanov <ivan.dr.ivanov@gmail.com>
2015-02-14 17:51:18 +0200

    Revert "Tested FindingDuplicateFiles::setDublicateFiles"
    This reverts commit d6d231664f72c4d9a8f4676f58a7ce20f8b08363.

--- src/main/FindingDuplicateFiles.java
+++ src/main/FindingDuplicateFiles.java
@@ -3,42 +3,21 @@ package main;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-
-import javax.swing.RowFilter.Entry;
 
 public class FindingDuplicateFiles {
-	// TODO:
-	// Opravi si construktorite!!!
-	// INACHE MECHKA!
-	
-	private HashMap<Long, ArrayList<File>> map;
+    private HashMap<Long, ArrayList<File>> map;
 
-	public void setDublicateFiles(ArrayList<File> allFiles) {
-		map = new HashMap<>();
-
-		for (File file : allFiles) {
-			if (map.containsKey(file.length())) {
-				map.get(file.length()).add(file);
-			} else {
-				ArrayList<File> arrayList = new ArrayList<>();
-				arrayList.add(file);
-				map.put(file.length(), arrayList);
-			}
-		}
-	}
-
-	public void print() {
-
-		for (Map.Entry<Long, ArrayList<File>> e : map.entrySet()) {
-			System.out.print("\t\tKey: " + e.getKey() + "\nValues:\n");
-			for (File f : e.getValue()) {
-				System.out.println(f.getAbsolutePath());
-			}
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-		}
-	}
+    private void setDublicateFiles(ArrayList<File> allFiles) {
+        map = new HashMap<>();
+        
+        for (File file: allFiles) {
+            if (map.containsKey(file.length())) {
+                map.get(file.length()).add(file);
+            } else {
+                ArrayList<File> arrayList = new ArrayList<>();
+                arrayList.add(file);
+                map.put(file.length(), arrayList);
+            }
+        }
+    }
 }
