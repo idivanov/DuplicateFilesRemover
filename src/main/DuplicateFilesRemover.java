d6d231664f72c4d9a8f4676f58a7ce20f8b08363
idivanov <ivan.dr.ivanov@gmail.com>
2015-02-14 17:49:17 +0200

    Tested FindingDuplicateFiles::setDublicateFiles
    It works

--- src/main/DuplicateFilesRemover.java
+++ src/main/DuplicateFilesRemover.java
@@ -6,16 +6,20 @@ import java.io.File;
 public class DuplicateFilesRemover {
 
 	public static void main(String[] args) {
-		 String basePath = "C:\\Users\\Mariyan\\Desktop\\test";
+		 String basePath = "C:\\Users\\User\\Desktop\\test";
 		//String basePath = "E:\\Music";
-		File file1 = new File(basePath + "\\1.mp3");
-		File file2 = new File(basePath + "\\2.mp3");
-//		DirectoryReader dr = new DirectoryReader(basePath);
-//		dr.go();
-		System.out.println(file1.hashCode() + " " + file2.hashCode() );
-		
-		file1.hashCode();
-		System.out.println(file1.length() + "\n" + file2.length() );
+//		File file1 = new File(basePath + "\\1.mp3");
+//		File file2 = new File(basePath + "\\2.mp3");	 
+//		System.out.println(file1.hashCode() + " " + file2.hashCode() );		
+//		file1.hashCode();
+//		System.out.println(file1.length() + "\n" + file2.length() );
+		 
+		DirectoryReader dr = new DirectoryReader(basePath);
+		dr.go();
+		FindingDuplicateFiles fdp = new FindingDuplicateFiles();
+		fdp.setDublicateFiles(dr.getFilesAndDirectories());
+		fdp.print();
+
 	}
 
 }
