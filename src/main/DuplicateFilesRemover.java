553fa73bf64cfbd8668710c871f65ca52b9e9b15
idivanov <ivan.dr.ivanov@gmail.com>
2015-02-14 15:54:30 +0200

    pull test

--- src/main/DuplicateFilesRemover.java
+++ src/main/DuplicateFilesRemover.java
@@ -4,6 +4,11 @@ import java.io.File;
 import java.util.Arrays;
 
 public class DuplicateFilesRemover {
+	
+	//
+	// Veche mojesh da pullvash uspeshno!
+	// gz
+	//
 
 	public static void main(String[] args) {
 		// String basePath = "C:\\Users\\User\\Desktop\\test";
